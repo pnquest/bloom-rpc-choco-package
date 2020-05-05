1384134d63743675707e4aa71c61a31c83858f82
pnquest <pn.quest@gmail.com>
2020-05-05 08:05:25 -0400

    comitting the first version of the package

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bloom-rpc-choco-package
-The chocolatey package for bloomrpc
+The chocolatey package for bloomRPC
+
+I am not a maintainer of [bloomRPC](https://github.com/uw-labs/bloomrpc) itself. I only maintain the [chocolatey](https://chocolatey.org/) package
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,44 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$version    = '1.4.1'
+$url        = "https://github.com/uw-labs/bloomrpc/releases/download/$version/bloomrpc-setup-$version.exe"
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'bloom-rpc*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs    = "/S `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
