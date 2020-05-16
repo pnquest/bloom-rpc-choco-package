a09c66092265d1e475f462153fcdeedbcdd3ad9d
pnquest <pn.quest@gmail.com>
2020-05-16 19:14:49 -0400

    adding checksum

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,23 +1,23 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+#These change with each version released
 $version    = '1.4.1'
+$checksum   = '465106850AE7C38921AF99E4E5D0CDBC8B6B3C4EFE3B6A7B88F2E1F9578D99B3'
+
 $url        = "https://github.com/uw-labs/bloomrpc/releases/download/$version/bloomrpc-setup-$version.exe"
-$url64      = ''
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
-  url64bit      = $url64
 
   softwareName  = 'bloom-rpc*'
 
-  checksum      = ''
+  checksum      = $checksum
   checksumType  = 'sha256'
-  checksum64    = ''
-  checksumType64= 'sha256'
 
   silentArgs    = "/S `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
