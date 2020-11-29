1e3b465e016f83f29eed2f80d2b75e756b8e31a9
pnquest <pn.quest@gmail.com>
2020-11-29 07:48:28 -0500

    update version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 #These change with each version released
-$version    = '1.4.1'
-$checksum   = '465106850AE7C38921AF99E4E5D0CDBC8B6B3C4EFE3B6A7B88F2E1F9578D99B3'
+$version    = '1.5.2'
+$checksum   = '29410F8608169BFAB91868BFB58B7BC89349D385DE1B6AE4FD7195079C1B625C'
 
 $url        = "https://github.com/uw-labs/bloomrpc/releases/download/$version/bloomrpc-setup-$version.exe"
 
